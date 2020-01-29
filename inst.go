3f2a7fc490c36bed5e0a33becaa6bc02989defb0
denk <d-enk@ya.ru>
2020-01-29 17:40:54 +0300

    inst begins

---
+++ inst.go
@@ -0,0 +1,367 @@
+package inst
+
+import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"errors"
+	"log"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+)
+
+const (
+	typeKEY    uint32 = 0
+	typeID     uint32 = 1
+	typeNextID uint32 = 2
+)
+
+var ccc int32
+
+// Inst /
+type Inst struct {
+	nameSpaces map[string]*NameSpaceMaps
+}
+
+// New NameSpaceMaps with ... string
+func New(etcdClient *clientv3.Client, prefix string, nameSpace ...string) *Inst {
+	s := &Inst{make(map[string]*NameSpaceMaps, len(nameSpace))}
+
+	NScounter := join(prefix, uint32ToString(typeNextID))
+	clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	etcdClient.Txn(clientCtx).
+		If(clientv3.Compare(clientv3.CreateRevision(NScounter), "=", 0)).
+		Then(clientv3.OpPut(NScounter, uint32ToString(1))).Commit()
+	cancel()
+
+	for i := range nameSpace {
+		s.nameSpaces[nameSpace[i]] = newInst(etcdClient, prefix, nameSpace[i])
+	}
+
+	return s
+}
+
+// Choose /
+func (s *Inst) Choose(nameSpace string) *NameSpaceMaps {
+	return s.nameSpaces[nameSpace]
+}
+
+// Shutdown close all
+func (s *Inst) Shutdown() {
+	for _, v := range s.nameSpaces {
+		v.Close()
+	}
+}
+
+// NameSpaceMaps /
+type NameSpaceMaps struct {
+	etcdClient  *clientv3.Client
+	prefix      string
+	nameSpace   string
+	nameSpaceID uint32
+
+	stopCntChan chan struct{}
+	stopKeyChan chan struct{}
+	counter     localCounter
+
+	atomic.Value
+	sync.Mutex
+}
+
+type maps struct {
+	strToID map[string]uint32
+	idToStr []*string
+}
+
+type localCounter struct {
+	num uint32
+	rev int64
+	sync.Mutex
+}
+
+// New version 2
+func newInst(etcdClient *clientv3.Client, prefix, nameSpace string) *NameSpaceMaps {
+	this := NameSpaceMaps{
+		etcdClient:  etcdClient,
+		prefix:      prefix,
+		nameSpace:   nameSpace,
+		nameSpaceID: 0,
+	}
+
+	this.checkNameSpaceID()
+	this.getNameSpaceValues()
+	return &this
+}
+
+func (s *NameSpaceMaps) checkNameSpaceID() {
+	NSname := join(s.prefix, s.nameSpace)
+	NScounter := join(s.prefix, uint32ToString(typeNextID))
+
+	clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	txnResp, err := s.etcdClient.Txn(clientCtx).
+		If(clientv3.Compare(clientv3.CreateRevision(NSname), "!=", 0)).
+		Then(clientv3.OpGet(NSname)).
+		Else(clientv3.OpGet(NScounter)).Commit()
+	cancel()
+	if err != nil {
+		log.Println(err)
+	}
+
+	s.nameSpaceID = byteToUInt32(txnResp.Responses[0].GetResponseRange().GetKvs()[0].Value)
+	if !txnResp.Succeeded {
+		for {
+			IDcounter := join(s.prefix, uint32ToString(s.nameSpaceID), uint32ToString(typeNextID))
+			modRevision := txnResp.Responses[0].GetResponseRange().GetKvs()[0].ModRevision
+
+			clientCtx, cancel = context.WithTimeout(context.Background(), 1000*time.Millisecond)
+			txnResp, err = s.etcdClient.Txn(clientCtx).
+				If(clientv3.Compare(clientv3.CreateRevision(NSname), "=", 0),
+					clientv3.Compare(clientv3.ModRevision(NScounter), "=", modRevision)).
+				Then(clientv3.OpPut(NSname, uint32ToString(s.nameSpaceID)),
+					clientv3.OpPut(NScounter, uint32ToString(s.nameSpaceID+1)),
+					clientv3.OpPut(IDcounter, uint32ToString(1))).
+				Else(clientv3.OpGet(NScounter),
+					clientv3.OpGet(NSname)).Commit()
+			cancel()
+			if err != nil {
+				log.Println(err)
+			}
+
+			if txnResp.Succeeded {
+				break
+			} else if r := txnResp.Responses[1].GetResponseRange(); r.Count == 1 {
+				s.nameSpaceID = byteToUInt32(r.GetKvs()[0].Value)
+				break
+			}
+			s.nameSpaceID = byteToUInt32(txnResp.Responses[0].GetResponseRange().GetKvs()[0].Value)
+		}
+	}
+}
+
+// Key string -> uint32
+func (s *NameSpaceMaps) Key(key string) (uint32, error) {
+	m := s.Load().(maps)
+	if m.strToID[key] == 0 {
+		return s.addKey(&key)
+	}
+	return m.strToID[key], nil
+}
+
+// ID uint32 -> string
+func (s *NameSpaceMaps) ID(id uint32) (*string, error) {
+	m := s.Load().(maps)
+	if id >= uint32(len(m.idToStr)) {
+		IDcounter := join(s.prefix, uint32ToString(s.nameSpaceID), uint32ToString(typeNextID))
+		clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+		getResp, err := s.etcdClient.Get(clientCtx, IDcounter)
+		cancel()
+		if err != nil {
+			return nil, err
+		}
+		if byteToUInt32(getResp.Kvs[0].Value) <= id {
+			return nil, errors.New("out of range")
+		}
+		time.Sleep(20 * time.Millisecond)
+		m := s.Load().(maps)
+		if id >= uint32(len(m.idToStr)) {
+			return nil, errors.New("timed out")
+		}
+	}
+	return m.idToStr[id], nil
+}
+
+// added key in etcd
+func (s *NameSpaceMaps) addKey(key *string) (uint32, error) {
+	KeyWithPrefix := join(s.prefix, uint32ToString(s.nameSpaceID), uint32ToString(typeKEY), *key)
+	IDcounter := join(s.prefix, uint32ToString(s.nameSpaceID), uint32ToString(typeNextID))
+
+	for {
+		s.counter.Lock()
+		ident := s.counter.num
+		modRevision := s.counter.rev
+		s.counter.Unlock()
+
+		log.Println("ccc", atomic.AddInt32(&ccc, 1))
+
+		clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+		txnResp, err := s.etcdClient.Txn(clientCtx).
+			If(clientv3.Compare(clientv3.CreateRevision(KeyWithPrefix), "=", 0),
+				clientv3.Compare(clientv3.ModRevision(IDcounter), "=", modRevision)).
+			Then(clientv3.OpPut(KeyWithPrefix, uint32ToString(ident)),
+				clientv3.OpPut(IDcounter, uint32ToString(ident+1))).
+			Else(clientv3.OpGet(IDcounter),
+				clientv3.OpGet(KeyWithPrefix)).Commit()
+		cancel()
+		if err != nil {
+			log.Println(err)
+			return 0, err
+		}
+
+		if txnResp.Succeeded {
+			s.localWrite(key, ident)
+			s.counter.Lock()
+			if ident+1 > s.counter.num {
+				s.counter.num = ident + 1
+				s.counter.rev = txnResp.Responses[1].GetResponsePut().Header.Revision
+			}
+			s.counter.Unlock()
+
+			return ident + 1, nil
+
+			// key found
+		} else if r := txnResp.Responses[1].GetResponseRange(); r.Count == 1 {
+			ident = byteToUInt32(r.GetKvs()[0].Value)
+			return ident, nil
+		}
+
+		s.counter.Lock()
+		if byteToUInt32(txnResp.Responses[0].GetResponseRange().GetKvs()[0].Value) > s.counter.num {
+			s.counter.num = byteToUInt32(txnResp.Responses[0].GetResponseRange().GetKvs()[0].Value)
+			s.counter.rev = txnResp.Responses[0].GetResponseRange().GetKvs()[0].ModRevision
+		}
+		s.counter.Unlock()
+	}
+
+}
+
+func (s *NameSpaceMaps) localWrite(str *string, id uint32) {
+	s.Lock()
+
+	m1 := s.Load().(maps)
+
+	l := uint32(len(m1.idToStr))
+	if l <= id {
+		l = id + 1
+	}
+
+	m2 := maps{make(map[string]uint32, len(m1.strToID)+1), make([]*string, l)}
+
+	for k, v := range m1.strToID {
+		m2.strToID[k] = v
+	}
+	m2.strToID[*str] = id
+
+	copy(m2.idToStr[:len(m1.idToStr)], m1.idToStr)
+	m2.idToStr[id] = str
+
+	s.Store(m2)
+
+	s.Unlock()
+}
+
+// GetNameSpaceValues /
+func (s *NameSpaceMaps) getNameSpaceValues() {
+	Key := join(s.prefix, uint32ToString(s.nameSpaceID), uint32ToString(typeKEY))
+	IDcounter := join(s.prefix, uint32ToString(s.nameSpaceID), uint32ToString(typeNextID))
+
+	newKeyChan := s.etcdClient.Watch(context.Background(), Key, clientv3.WithPrefix())
+	newCntChan := s.etcdClient.Watch(context.Background(), IDcounter)
+
+	clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	resp, err := s.etcdClient.Txn(clientCtx).
+		Then(clientv3.OpGet(Key, clientv3.WithPrefix()),
+			clientv3.OpGet(IDcounter)).Commit()
+	cancel()
+	if err != nil {
+		log.Println(err)
+	}
+
+	r := resp.Responses[0].GetResponseRange()
+	mp := maps{make(map[string]uint32, r.Count), make([]*string, r.Count+1)}
+	for _, v := range r.Kvs {
+		str := string(v.Key[len(Key):])
+		id := byteToUInt32(v.Value)
+		mp.strToID[str] = id
+		mp.idToStr[id] = &str
+	}
+
+	r = resp.Responses[1].GetResponseRange()
+
+	s.counter = localCounter{
+		num: byteToUInt32(r.GetKvs()[0].Value),
+		rev: r.GetKvs()[0].ModRevision,
+	}
+
+	s.Store(mp)
+
+	go func() {
+		for {
+			select {
+			case wresp := <-newKeyChan:
+				for _, ev := range wresp.Events {
+					if ev.Type == mvccpb.PUT {
+						id := byteToUInt32(ev.Kv.Value)
+						sl := s.Load().(maps)
+						if !(uint32(len(sl.idToStr)) > id && sl.idToStr[id] != nil) {
+							str := string(ev.Kv.Key[len(Key):])
+							s.localWrite(&str, id)
+						}
+					}
+				}
+
+			case <-s.stopKeyChan:
+				close(s.stopKeyChan)
+				return
+
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case wresp := <-newCntChan:
+				for _, ev := range wresp.Events {
+					if ev.Type == mvccpb.PUT {
+						s.counter.Lock()
+						if byteToUInt32(ev.Kv.Value) > s.counter.num {
+							s.counter.num = byteToUInt32(ev.Kv.Value)
+							s.counter.rev = ev.Kv.ModRevision
+						}
+						s.counter.Unlock()
+					}
+				}
+
+			case <-s.stopCntChan:
+				close(s.stopCntChan)
+				return
+
+			}
+		}
+	}()
+}
+
+// Close /
+func (s *NameSpaceMaps) Close() {
+	s.stopCntChan <- struct{}{}
+	s.stopKeyChan <- struct{}{}
+}
+
+func uint32ToString(num uint32) string {
+	buf := &bytes.Buffer{}
+
+	err := binary.Write(buf, binary.BigEndian, num)
+	if err != nil {
+		log.Fatal(err)
+		return ""
+	}
+
+	return buf.String()
+}
+
+func byteToUInt32(b []byte) uint32 {
+	return binary.BigEndian.Uint32(b)
+}
+
+func join(strs ...string) string {
+	var sb strings.Builder
+	for _, str := range strs {
+		sb.WriteString(str)
+	}
+	return sb.String()
+}
---
+++ inst_test.go
@@ -0,0 +1,615 @@
+package inst
+
+import (
+	"context"
+	"log"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+var testPrefix = "test"
+
+func newServer() {
+
+}
+
+func newConnect() *clientv3.Client {
+	etcdCli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return etcdCli
+}
+
+func erase(cl *clientv3.Client) {
+	clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	cl.Delete(clientCtx, testPrefix, clientv3.WithPrefix())
+	cancel()
+}
+
+func count(cl *clientv3.Client, key string) int64 {
+	clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	p, _ := cl.Get(clientCtx, testPrefix+key, clientv3.WithPrefix())
+	cancel()
+	return p.Count
+}
+
+func get(cl *clientv3.Client, key string) *clientv3.GetResponse {
+	clientCtx, cancel := context.WithTimeout(context.Background(), 1000*time.Millisecond)
+	p, _ := cl.Get(clientCtx, testPrefix+key, clientv3.WithPrefix())
+	cancel()
+	return p
+}
+
+func Test1(t *testing.T) {
+	// t.Error()
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+	//defer erase(cl)
+	New(cl, testPrefix, "000")
+
+	if count(cl, "") != 3 { //NS counter, NameSpace and keys counter
+		t.Error()
+	}
+}
+
+func Test2(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+	//	defer erase(cl)
+
+	New(cl, testPrefix, "000")
+	New(cl, testPrefix, "001")
+
+	if count(cl, "") != 5 { // NS counter, 2 NameSpaces and 2 keys counters
+		t.Error()
+	}
+
+	New(cl, testPrefix, "002")
+	New(cl, testPrefix, "003")
+	// time.Sleep(10 * time.Millisecond)
+
+	if count(cl, "") != 9 { // NS counter, 4 NameSpaces and 4 keys counter
+		t.Error()
+	}
+	time.Sleep(2000)
+}
+
+func Test3(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+
+	//defer erase(cl)
+
+	var wg sync.WaitGroup
+	f := func(b, e int) {
+		defer wg.Done()
+		for i := b; i < e; i++ {
+			New(cl, testPrefix, strconv.Itoa(i))
+		}
+	}
+
+	wg.Add(2)
+	f(0, 50)
+	f(10, 100)
+
+	// time.Sleep(100)
+	wg.Wait()
+
+	if c := count(cl, ""); c != 201 { // 1 NS counter, 1000 NameSpace and 1000 keys counter
+		log.Println(c)
+		t.Error()
+	}
+	time.Sleep(2000)
+}
+
+func Test4(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+
+	// defer erase(cl)
+
+	s := New(cl, testPrefix, "0001")
+	s.Choose("0001").Key("1")
+
+	if c := count(cl, uint32ToString(s.Choose("0001").nameSpaceID)); c != 2 { // 1 key and IDs counter
+		t.Error()
+	}
+
+	if c := count(cl, uint32ToString(s.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)); c != 1 { // 1 key
+		t.Error()
+	}
+
+	if p := get(cl, uint32ToString(s.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)+"1"); string(p.Kvs[0].Value) != uint32ToString(1) {
+		t.Error()
+	}
+
+}
+
+func Test5(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+
+	// defer erase(cl)
+
+	var wg sync.WaitGroup
+	s := New(cl, testPrefix, "0001")
+
+	f := func(b, e int) {
+		for i := b; i < e; i++ {
+			s.Choose("0001").Key(strconv.Itoa(i))
+		}
+		wg.Done()
+	}
+	wg.Add(2)
+	go f(0, 1000)
+	go f(500, 1500)
+
+	wg.Wait()
+
+	if c := count(cl, ""); c != 1503 { // 1500 keys and 1 ID counter,1 NameSpace and NS counter
+		log.Println(c)
+		t.Error()
+	}
+
+	if c := count(cl, uint32ToString(s.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)); c != 1500 { // 1500 keys
+		t.Error()
+	}
+
+}
+
+func Test6(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+
+	// defer erase(cl)
+
+	var wg sync.WaitGroup
+	s1 := New(cl, testPrefix, "0001")
+	s2 := New(cl, testPrefix, "0002")
+
+	f := func(b, e int) {
+		for i := b; i < e; i++ {
+			s1.Choose("0001").Key(strconv.Itoa(i))
+			s2.Choose("0002").Key(strconv.Itoa(i))
+		}
+		wg.Done()
+	}
+	wg.Add(2)
+	go f(0, 1000)
+	go f(500, 1500)
+
+	wg.Wait()
+
+	if c := count(cl, ""); c != 3005 { // 2x1500 keys and 2 ID counter, 2 NameSpace and NS counter
+		t.Error()
+	}
+
+	if c := count(cl, uint32ToString(s1.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)); c != 1500 { // 1500 keys
+		t.Error()
+	}
+
+	if c := count(cl, uint32ToString(s2.Choose("0002").nameSpaceID)+uint32ToString(typeKEY)); c != 1500 { // 1500 keys
+		t.Error()
+	}
+}
+
+func Test7(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+
+	// defer erase(cl)
+
+	s1 := New(cl, testPrefix, "0001")
+
+	f := func(s *Inst, b, e int) {
+		for i := b; i < e; i++ {
+			s.Choose("0001").Key(strconv.Itoa(i))
+		}
+	}
+
+	f(s1, 1000, 1010)
+	// localWrite
+	m := s1.Choose("0001").Load().(maps).strToID
+	k := s1.Choose("0001").Load().(maps).idToStr
+
+	if m["1000"] != 1 || *k[1] != "1000" {
+		t.Error()
+	}
+	if m["1002"] != 3 || *k[3] != "1002" {
+		t.Error()
+	}
+
+	// standart init
+	s1 = New(cl, testPrefix, "0001")
+	m = s1.Choose("0001").Load().(maps).strToID
+	k = s1.Choose("0001").Load().(maps).idToStr
+
+	if m["1000"] != 1 || *k[1] != "1000" {
+		t.Error()
+	}
+	if m["1002"] != 3 || *k[3] != "1002" {
+		t.Error()
+	}
+	if m["1006"] != 7 || *k[7] != "1006" {
+		t.Error()
+	}
+	if m["1009"] != 10 || *k[10] != "1009" {
+		t.Error()
+	}
+
+	// watcher check
+	s2 := New(cl, testPrefix, "0001")
+	f(s2, 1010, 1020)
+	time.Sleep(10 * time.Millisecond)
+	m = s1.Choose("0001").Load().(maps).strToID
+	k = s1.Choose("0001").Load().(maps).idToStr
+
+	id, _ := s1.Choose("0001").Key("1010")
+	st, _ := s1.Choose("0001").ID(11)
+	if m["1010"] != 11 || *k[11] != "1010" || id != 11 || *st != "1010" {
+		t.Error()
+	}
+
+	if m["1015"] != 16 || *k[16] != "1015" {
+		t.Error()
+	}
+}
+
+func Test8(t *testing.T) {
+	cl := newConnect()
+	defer cl.Close()
+	erase(cl)
+
+	// defer erase(cl)
+
+	var wg sync.WaitGroup
+	s1 := New(cl, testPrefix, "0001")
+
+	f := func(s *Inst, b, e int) {
+		for i := b; i < e; i++ {
+			s.Choose("0001").Key(strconv.Itoa(i))
+		}
+		wg.Done()
+	}
+
+	wg.Add(9)
+	go f(s1, 1000, 2500)
+	go f(s1, 1000, 2500)
+	go f(s1, 3000, 4500)
+	go f(s1, 2000, 3500)
+	go f(s1, 2000, 5500)
+	go f(s1, 2000, 5500)
+	go f(s1, 0, 1000)
+	go f(s1, 5000, 10000)
+	go f(s1, 3000, 4500)
+
+	wg.Wait()
+	// localWrite
+	m := s1.Choose("0001").Load().(maps).strToID
+	k := s1.Choose("0001").Load().(maps).idToStr
+
+	if len(m) != 10000 || len(k) != 10001 {
+		log.Println(len(m), len(k))
+		t.Error()
+	}
+
+	for i, v := range m {
+		if *k[v] != i {
+			t.Error()
+		}
+	}
+
+	for i, v := range k[1:] {
+		if v == nil {
+			t.Error()
+		}
+
+		if m[*v] != uint32(i+1) {
+			t.Error()
+		}
+
+	}
+}
+
+func TestAll(t *testing.T) {
+	{
+		// Test 1
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+		//defer erase(cl)
+		New(cl, testPrefix, "000")
+
+		if count(cl, "") != 3 { //NS counter, NameSpace and keys counter
+			t.Error()
+		}
+	}
+
+	{
+		// Test 2
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+		//	defer erase(cl)
+
+		New(cl, testPrefix, "000")
+		New(cl, testPrefix, "001")
+
+		if count(cl, "") != 5 { // NS counter, 2 NameSpaces and 2 keys counters
+			t.Error()
+		}
+
+		New(cl, testPrefix, "002")
+		New(cl, testPrefix, "003")
+		// time.Sleep(10 * time.Millisecond)
+
+		if count(cl, "") != 9 { // NS counter, 4 NameSpaces and 4 keys counter
+			t.Error()
+		}
+		time.Sleep(2000)
+	}
+
+	{
+		// Test 3
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+
+		//defer erase(cl)
+
+		var wg sync.WaitGroup
+		f := func(b, e int) {
+			defer wg.Done()
+			for i := b; i < e; i++ {
+				New(cl, testPrefix, strconv.Itoa(i))
+			}
+		}
+
+		wg.Add(2)
+		f(0, 50)
+		f(10, 100)
+
+		// time.Sleep(100)
+		wg.Wait()
+
+		if c := count(cl, ""); c != 201 { // 1 NS counter, 1000 NameSpace and 1000 keys counter
+			log.Println(c)
+			t.Error()
+		}
+		time.Sleep(2000)
+	}
+
+	{
+		// Test 4
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+
+		defer erase(cl)
+
+		s := New(cl, testPrefix, "0001")
+		s.Choose("0001").Key("1")
+
+		if c := count(cl, uint32ToString(s.Choose("0001").nameSpaceID)); c != 2 { // 1 key and IDs counter
+			t.Error()
+		}
+
+		if c := count(cl, uint32ToString(s.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)); c != 1 { // 1 key
+			t.Error()
+		}
+
+		if p := get(cl, uint32ToString(s.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)+"1"); string(p.Kvs[0].Value) != uint32ToString(1) {
+			t.Error()
+		}
+
+	}
+
+	{
+		// Test 5
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+
+		// defer erase(cl)
+
+		var wg sync.WaitGroup
+		s := New(cl, testPrefix, "0001")
+
+		f := func(b, e int) {
+			for i := b; i < e; i++ {
+				s.Choose("0001").Key(strconv.Itoa(i))
+			}
+			wg.Done()
+		}
+		wg.Add(2)
+		go f(0, 1000)
+		go f(500, 1500)
+
+		wg.Wait()
+
+		if c := count(cl, ""); c != 1503 { // 1500 keys and 1 ID counter,1 NameSpace and NS counter
+			log.Println(c)
+			t.Error()
+		}
+
+		if c := count(cl, uint32ToString(s.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)); c != 1500 { // 1500 keys
+			t.Error()
+		}
+
+	}
+
+	{
+		// Test 6
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+
+		// defer erase(cl)
+
+		var wg sync.WaitGroup
+		s1 := New(cl, testPrefix, "0001")
+		s2 := New(cl, testPrefix, "0002")
+
+		f := func(b, e int) {
+			defer wg.Done()
+			for i := b; i < e; i++ {
+				s1.Choose("0001").Key(strconv.Itoa(i))
+				s2.Choose("0002").Key(strconv.Itoa(i))
+			}
+		}
+		wg.Add(2)
+		go f(0, 1000)
+		go f(500, 1500)
+
+		wg.Wait()
+
+		if c := count(cl, ""); c != 3005 { // 2x1500 keys and 2 ID counter, 2 NameSpace and NS counter
+			t.Error()
+		}
+
+		if c := count(cl, uint32ToString(s1.Choose("0001").nameSpaceID)+uint32ToString(typeKEY)); c != 1500 { // 1500 keys
+			t.Error()
+		}
+
+		if c := count(cl, uint32ToString(s2.Choose("0002").nameSpaceID)+uint32ToString(typeKEY)); c != 1500 { // 1500 keys
+			t.Error()
+		}
+	}
+
+	{
+		// Test 7
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+
+		// defer erase(cl)
+
+		s1 := New(cl, testPrefix, "0001")
+
+		f := func(s *Inst, b, e int) {
+			for i := b; i < e; i++ {
+				s.Choose("0001").Key(strconv.Itoa(i))
+			}
+		}
+
+		f(s1, 1000, 1010)
+		// localWrite
+		m := s1.Choose("0001").Load().(maps).strToID
+		k := s1.Choose("0001").Load().(maps).idToStr
+
+		if m["1000"] != 1 || *k[1] != "1000" {
+			t.Error()
+		}
+		if m["1002"] != 3 || *k[3] != "1002" {
+			t.Error()
+		}
+
+		// standart init
+		s1 = New(cl, testPrefix, "0001")
+		m = s1.Choose("0001").Load().(maps).strToID
+		k = s1.Choose("0001").Load().(maps).idToStr
+
+		if m["1000"] != 1 || *k[1] != "1000" {
+			t.Error()
+		}
+		if m["1002"] != 3 || *k[3] != "1002" {
+			t.Error()
+		}
+		if m["1006"] != 7 || *k[7] != "1006" {
+			t.Error()
+		}
+		if m["1009"] != 10 || *k[10] != "1009" {
+			t.Error()
+		}
+
+		// watcher check
+		s2 := New(cl, testPrefix, "0001")
+		f(s2, 1010, 1020)
+		time.Sleep(10 * time.Millisecond)
+		m = s1.Choose("0001").Load().(maps).strToID
+		k = s1.Choose("0001").Load().(maps).idToStr
+
+		id, _ := s1.Choose("0001").Key("1010")
+		st, _ := s1.Choose("0001").ID(11)
+		if m["1010"] != 11 || *k[11] != "1010" || id != 11 || *st != "1010" {
+			t.Error()
+		}
+
+		if m["1015"] != 16 || *k[16] != "1015" {
+			t.Error()
+		}
+	}
+
+	{
+		// Test 8
+		cl := newConnect()
+		defer cl.Close()
+		erase(cl)
+
+		// defer erase(cl)
+
+		var wg sync.WaitGroup
+		s1 := New(cl, testPrefix, "0001")
+
+		f := func(s *Inst, b, e int) {
+			defer wg.Done()
+			for i := b; i < e; i++ {
+				s.Choose("0001").Key(strconv.Itoa(i))
+			}
+		}
+
+		wg.Add(9)
+		go f(s1, 1000, 2500)
+		go f(s1, 1000, 2500)
+		go f(s1, 3000, 4500)
+		go f(s1, 2000, 3500)
+		go f(s1, 2000, 5500)
+		go f(s1, 2000, 5500)
+		go f(s1, 0, 1000)
+		go f(s1, 5000, 10000)
+		go f(s1, 3000, 4500)
+
+		wg.Wait()
+		// localWrite
+		m := s1.Choose("0001").Load().(maps).strToID
+		k := s1.Choose("0001").Load().(maps).idToStr
+
+		if len(m) != 10000 || len(k) != 10001 {
+			log.Println(len(m), len(k))
+			t.Error()
+		}
+
+		for i, v := range m {
+			if *k[v] != i {
+				t.Error()
+			}
+		}
+
+		for i, v := range k[1:] {
+			if v == nil {
+				t.Error()
+			}
+
+			if m[*v] != uint32(i+1) {
+				t.Error()
+			}
+
+		}
+	}
+}
